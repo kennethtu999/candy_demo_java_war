9400946c1361ce383e2aa182802886732e0c4052
kenneth <kenneth.tu@gmail.com>
2017-05-13 23:54:49 +0800

    upgrade springboot

--- src/main/java/sample/war/SampleWarApplication.java
+++ src/main/java/sample/war/SampleWarApplication.java
@@ -18,7 +18,7 @@ package sample.war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SampleWarApplication extends SpringBootServletInitializer {
