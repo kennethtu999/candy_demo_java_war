50a435ea904145dc04077ee4042d0f35fe8d62d9
kenneth <kenneth.tu@gmail.com>
2017-05-16 08:13:54 +0800

    modify pipeline config

--- src/main/java/sample/war/SimpleErrorController.java
+++ src/main/java/sample/war/SimpleErrorController.java
@@ -13,7 +13,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/error")
 public class SimpleErrorController implements ErrorController {
 
